81273a5757e43d7d016585729eded8cd778796d0
jhew86 <jhew86@gmail.com>
2016-03-30 12:18:10 -0400

    Added files via upload

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+"""
+Palindrome evaluator
+by: jhew86
+"""
+
+lst1=["bat", "cat", "hat", "cat", "bat"]
+rvs=lst1[::-1]
+if lst1== rvs:
+    print("this list is a palindrome")
+else:
+    print("this list is not a palindrome")
